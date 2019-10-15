e0f96f54f21b39643a271872f008930a07f19337
sally <kennethzhang374@gmail.com>
2019-10-15 18:21:33 +0000

    finished exercises 1,2,3 creating, updating and destroying database attributes

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,13 @@
 require_relative '../setup'
+Surrey (annual_revenue of 224000, carries women's apparel only)
+Whistler (annual_revenue of 1900000 carries men's apparel only)
+Yaletown (annual_revenue of 430000 carries men's and women's apparel)
 
 puts "Exercise 1"
 puts "----------"
+Store.create(name: "Burnaby", annual_revenue:300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue:1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue:190000, mens_apparel: true, womens_apparel: false)
 
-# Your code goes below here ...
+puts Store.count
+
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,11 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
+
 puts "Exercise 2"
 puts "----------"
+@store1 = Store.find(1)
+@store2 = Store.find(2)
 
-# Your code goes here ...
+@store1.update(name: 'Burnaby Reborn')
+
